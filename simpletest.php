48fdd2995db1f5d26c9a8b0f377028518ee39df6
Eric Hansen <hansen.c.eric@gmail.com>
2014-09-07 14:25:14 -0400

    Added method to get all notifiers, updated simpletest to reflect how to use plus some semi-useful changes

--- simpletest.php
+++ simpletest.php
@@ -1,12 +1,13 @@
 <?php
-/**
- * Uncomment if not using Composer's auto-loader:
- *
- * @require_once "src/EricHansen/Notifier/Notifier.php";
- */
+if(file_exists('vendor/autoload.php'))
+    @require_once 'vendor/autoload.php';
+else
+    @require_once "src/EricHansen/Notifier/Notifier.php";
 
 // Load up the class and initialize it (nothing to do there)
 $n = new \EricHansen\Notifier\Notifier();
 
-// Only works if libnotify is installed on the system.  Otherwise, nothing will show.
-$n->Notify(array("title" => "Test", "msg" => "Stupid"));
+foreach($n->getAllNotifiers() as $notifier){
+    $n->$notifier(array("title" => "Test", "msg" => "Works!"));
+    sleep(5);
+}
--- src/EricHansen/Notifier/Notifier.php
+++ src/EricHansen/Notifier/Notifier.php
@@ -3,12 +3,54 @@ namespace EricHansen\Notifier;
 
 class Notifier {
     /**
+     * @param mixed $notifiers_path If given, uses this path to load up notifier classes (defaults ./notifiers)
+     */
+    public function __construct($notifiers_path=null){
+        if(!$notifiers_path) {
+            $this->notifiers_path = dirname(__FILE__) . DIRECTORY_SEPARATOR . "notifiers" . DIRECTORY_SEPARATOR;
+        } else {
+            $this->notifiers_path = $notifiers_path;
+        }
+    }
+
+    /**
+     * Returns a list of all the notifiers detected (must extended NotifierBase to be detected)
+     *
+     * @return array An array of valid notifiers that were detected
+     */
+    public function getAllNotifiers(){
+        $content = scandir($this->notifiers_path);
+
+        $notifiers = array();
+
+        foreach($content as $file){
+            if($file[0] == ".")
+                continue;
+
+            $clsname = basename($file, ".php");
+
+            if(!class_exists($clsname))
+                @require_once $this->notifiers_path . $file;
+
+
+            $tmp = new \ReflectionClass(__NAMESPACE__ . "\\Notifiers\\" . $clsname);
+
+            if($tmp->isSubclassOf(__NAMESPACE__ . "\\NotifierBase"))
+                $notifiers[] = $clsname;
+
+            unset($clsname);
+        }
+
+        return $notifiers;
+    }
+
+    /**
      * @param $method string The notifier to load
      * @param $args array Any arguments to send to the notifier
      * @return bool Success or failure (true or false)
      */
     public function __call($method, $args){
-        $file = dirname(__FILE__) . DIRECTORY_SEPARATOR . "notifiers" . DIRECTORY_SEPARATOR . $method . ".php";
+        $file = $this->notifiers_path . $method . ".php";
 
         $notifier = null;
 
